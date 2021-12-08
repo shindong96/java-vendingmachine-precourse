fd03dc42262f7a054378d34d9e0e1327b3c0aaa5
shindong96 <2006peter@naver.com>
2021-12-08 19:54:01 +0900

    feat(Coin) : 잔돈 동전 갯수를 랜덤하게 준비하는 기능 구현

--- src/main/java/vendingmachine/Coin.java
+++ src/main/java/vendingmachine/Coin.java
@@ -1,16 +1,40 @@
 package vendingmachine;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
 public enum Coin {
-    COIN_500(500),
-    COIN_100(100),
-    COIN_50(50),
-    COIN_10(10);
+	COIN_500(500),
+	COIN_100(100),
+	COIN_50(50),
+	COIN_10(10);
 
-    private final int amount;
+	private final int amount;
 
-    Coin(final int amount) {
-        this.amount = amount;
-    }
+	Coin(final int amount) {
+		this.amount = amount;
+	}
 
-    // 추가 기능 구현
+	public int prepareChanges(int money, List<Integer> numberOfCoins) {
+		int limit = money / amount;
+		List<Integer> randomNumberList = makeRandomNumberList(limit);
+		int numberOfCoin = Randoms.pickNumberInList(randomNumberList);
+		numberOfCoins.add(numberOfCoin);
+
+		return money - numberOfCoin * amount;
+	}
+
+	private List<Integer> makeRandomNumberList(int limit) {
+		List<Integer> numberList = new ArrayList<Integer>();
+
+		for (int i = 0; i <= limit; i++) {
+			numberList.add(i);
+		}
+
+		return numberList;
+	}
+
+	// 추가 기능 구현
 }
